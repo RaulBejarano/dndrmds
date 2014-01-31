bc6f38a1bcd0a8573181d348f4e234fd2b29a729
RaulBejarano <raulbejaranoparrilla@gmail.com>
2014-01-31 20:52:35 +0100

    Cambios

--- Android/src/dandremids/src/model/db/Creature_Attack.java
+++
@@ -1,9 +0,0 @@
-package dandremids.src.model.db;
-
-public class Creature_Attack {
-	public int Attack_id;
-	public int Creature_id;
-	public int level;
-	public int uses;
-	public int nextLevelUses;
-}
--- Android/src/dandremids/src/model/db/Creature_State.java
+++
@@ -1,6 +0,0 @@
-package dandremids.src.model.db;
-
-public class Creature_State {
-	public int State_id;
-	public int Creature_id;
-}
